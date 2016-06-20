a8f6bbf92bc30e3c90d1276d6c51f372d759fe2a
Antonio Scaramazza <tonyaleja@gmail.com>
2016-06-20 12:06:19 -0400

    Base files

---
+++ OgrePlantillaProyectoVII/Ogre_Plantilla_01/mainProyecto.cpp
@@ -0,0 +1,113 @@
+#include "Ogre\ExampleApplication.h"
+
+class Example1 : public ExampleApplication
+{
+
+public:
+
+	void createCamera() {
+
+		mCamera = mSceneMgr->createCamera("MyCamera1");
+		mCamera->setPosition(0,100,-200);
+		mCamera->lookAt(0,0,100);
+		mCamera->setNearClipDistance(1);
+
+	}
+
+	void createScene()
+	{
+
+		mSceneMgr->setAmbientLight(Ogre::ColourValue(1.0, 1.0, 1.0));
+		mSceneMgr->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
+		
+		Ogre::Light* LuzPuntual01 = mSceneMgr->createLight("Luz01");
+		LuzPuntual01->setType(Ogre::Light::LT_DIRECTIONAL);
+		LuzPuntual01->setDiffuseColour(1.0,1.0,1.0);
+		LuzPuntual01->setDirection(Ogre::Vector3( 1, -1, -1 ));
+		
+		Ogre::Light* LuzPuntual02 = mSceneMgr->createLight("Luz02");
+		LuzPuntual02->setType(Ogre::Light::LT_DIRECTIONAL);
+		LuzPuntual02->setDiffuseColour(1.0,1.0,1.0);
+		LuzPuntual02->setDirection(Ogre::Vector3( -1, -1, -1 ));
+
+		//Rueda
+		Ogre::SceneNode* _nodeRueda01 = mSceneMgr->createSceneNode("Rueda01");
+		mSceneMgr->getRootSceneNode()->addChild(_nodeRueda01);
+			
+		Ogre::Entity* _entRueda01 = mSceneMgr->createEntity("entRueda01", "ruedaDetallada.mesh");
+		_nodeRueda01->translate(-5.77,3.517,-9.462);
+		_entRueda01->setMaterialName("shRueda02");
+		_nodeRueda01->attachObject(_entRueda01);
+
+		//Chasis
+		Ogre::SceneNode* _nodeChasis01 = mSceneMgr->createSceneNode("Chasis01");
+		mSceneMgr->getRootSceneNode()->addChild(_nodeChasis01);
+			
+		Ogre::Entity* _entChasis01 = mSceneMgr->createEntity("entChasis01", "chasisCarro.mesh");
+		_entChasis01->setMaterialName("shCarro01");
+		_nodeChasis01->attachObject(_entChasis01);
+
+
+		//BordePista
+		Ogre::SceneNode* _nodeBPista = mSceneMgr->createSceneNode("BordePista");
+		mSceneMgr->getRootSceneNode()->addChild(_nodeBPista);
+				
+		Ogre::Entity* _entBPista = mSceneMgr->createEntity("BordePista01", "bordePista.mesh");
+		_nodeBPista->attachObject(_entBPista);
+
+
+		//PisoObstaculo
+		Ogre::SceneNode* _nodePObstaculo = mSceneMgr->createSceneNode("PistaObstaculo");
+		mSceneMgr->getRootSceneNode()->addChild(_nodePObstaculo);
+				
+		Ogre::Entity* _entPObstaculo = mSceneMgr->createEntity("PistaObstaculo", "pisoObstaculo01.mesh");
+		_nodePObstaculo->attachObject(_entPObstaculo);
+
+		//PisoNOObstaculo
+		Ogre::SceneNode* _nodePNObstaculo = mSceneMgr->createSceneNode("PistaNoObstaculo");
+		mSceneMgr->getRootSceneNode()->addChild(_nodePNObstaculo);
+				
+		Ogre::Entity* _entPNOObstaculo = mSceneMgr->createEntity("PistaNoObstaculo", "pisoNoObstaculo01.mesh");
+		_nodePNObstaculo->attachObject(_entPNOObstaculo);
+
+
+		//PosterInicioFinal
+		Ogre::SceneNode* _nodePoster = mSceneMgr->createSceneNode("PosterInicioFinal");
+		mSceneMgr->getRootSceneNode()->addChild(_nodePoster);
+			
+		Ogre::Entity* _entPoster = mSceneMgr->createEntity("PosterInicioFinal", "posterInicioFinal.mesh");
+		_nodePoster->attachObject(_entPoster);
+
+				
+		//BanderaInicial
+		Ogre::SceneNode* _nodeBInicial = mSceneMgr->createSceneNode("BanderaInicial");
+		mSceneMgr->getRootSceneNode()->addChild(_nodeBInicial);
+				
+		Ogre::Entity* _entBanderaI = mSceneMgr->createEntity("BanderaInicial", "banderaInicial.mesh");				
+		_entBanderaI->setMaterialName("lambert1");				
+		_nodeBInicial->attachObject(_entBanderaI);
+
+
+		//BanderaFinal
+		Ogre::SceneNode* _nodeBFinal = mSceneMgr->createSceneNode("BanderaFinal");
+		mSceneMgr->getRootSceneNode()->addChild(_nodeBFinal);
+		
+		Ogre::Entity* _entBanderaF = mSceneMgr->createEntity("BanderaFinal", "banderaFinal.mesh");
+		_entBanderaF->setMaterialName("lambert1");
+		_nodeBFinal->attachObject(_entBanderaF);
+
+
+
+
+	}
+
+};
+
+
+int main (void)
+{
+
+	Example1 app;
+	app.go();
+	return 0; 
+}
